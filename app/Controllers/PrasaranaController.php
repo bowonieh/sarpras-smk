dc5dd74f45f2f8e8735725ac4870a0fcd5272618
Agus Wibowo <aguswibowo.amd@gmail.com>
2021-03-06 00:10:01 +0700

    l

--- app/Controllers/PrasaranaController.php
+++ app/Controllers/PrasaranaController.php
@@ -15,6 +15,7 @@ class PrasaranaController extends BaseController
 	{
 		$this->jns_ruang = new RuangModel;
 		$this->prasarana = new PrasaranaModel;
+		helper('App');
 	}
 	public function index()
 	{
@@ -28,7 +29,95 @@ class PrasaranaController extends BaseController
 		return view('master/prasarana/tambah',$data);
 	}
 	public function simpan(){
+		$data = [
+			'id_kk'	=> $this->request->getPost('kompetensi_keahlian'),
+			'id_area'	=> $this->request->getPost('id_area'),
+			'nama_ruang'	=> $this->request->getPost('nama_ruang'),
+			'panjang'	=> $this->request->getPost('panjang'),
+			'lebar'		=> $this->request->getPost('lebar')
+		];
+		if(!empty($this->request->getPost('id_ruang'))){
+			$data = array_push_assoc($data,'id_ruang',$this->request->getPost('id_ruang'));
+		}
+		$rules = [
+				'nama_ruang' => array(
+					'rules' => 'required',
+					'errors' => array(
+						'required' => 'Ruang Area tidak boleh kosong',
+					)
+				),
+				'kompetensi_keahlian' => array(
+					'rules'	=> 'required',
+					'errors' => array(
+						'required'	=> 'Kompetensi Keahlian belum dipilih'
+					)
+				),
+				'id_area' => array(
+					'rules'	=> 'required',
+					'errors' => array(
+						'required'	=> 'Jenis Ruangan belum dipilih'
+					)
+				)
+					
 
+		];
+		
+		if($this->validate($rules)):
+			if(!empty($this->request->getFile('file')->getName())){
+				$validasi = $this->validate(
+					array(
+						'file' => [
+							'uploaded[file]',
+							'mime_in[file,image/jpg,image/jpeg,image/gif,image/png]',
+							'max_size[file,4096]',
+						]
+					)
+				);
+				$file = $this->request->getFile('file');
+				if($validasi){
+					//Upload File
+					$file_name = $file->getRandomName();
+							$file->move('./uploads/gambaruang/',$file_name);
+                            $data = array_push_assoc($data,'gambar',$file_name);
+							$a = $this->prasarana->save($data);
+							if($a){
+								$output = [
+									'success' => true,
+									'pesan'	 => 'Upload gambar berhasil, Transaksi Berhasil'
+								];
+							}else{
+								$output =[
+									'success'	=> false,
+									'pesan'		=> 'Upload gambar berhasil, Transaksi gagal'
+								];
+							}
+				}else{
+					$output =[
+						'success'	=> false,
+						'pesan'		=> 'Upload Berkas Gagal'
+					];
+				}	
+			}else{
+				$a = $this->prasarana->save($data);
+					if($a){
+						$output = [
+							'success' => true,
+							'pesan'	 => 'Transaksi Berhasil'
+						];
+					}else{
+						$output =[
+							'success'	=> false,
+							'pesan'		=> 'Transaksi gagal'
+						];
+					}
+			}
+		else:
+			$output =[
+				'success'	=> false,
+				'pesan'		=> 'General Errors: #1 Transaksi gagal. Mohon lengkapi isian'
+			];
+		endif;
+		return $this->response->setJSON($output);
 	}
 	public function edit($id = null){
 
---
+++ app/Views/master/prasarana/tambah.php
@@ -0,0 +1,113 @@
+<?php $this->extend('layout/header')?>
+<?php $this->section('content')?>
+<section class="content">
+    
+      <!-- Main row -->
+      <div class="row">
+        <section class="col-lg-12">
+        <div class="box box-success">
+            <div class="box-header">
+              <i class="fa fa-form"></i>
+              <h3 class="box-title">Form Tambah Data</h3>
+            </div>
+            <div class="box-body">
+            <form id="formTambahData" enctype="multipart/form-data">
+              <div class="box-body">
+              
+                <div class="form-group">
+                  <label for="kompetensiKeahlian">KOMPETENSI KEAHLIAN</label>
+                  <select name='kompetensi_keahlian' class="form-control kompetensi_keahlian"></select>
+                  
+                </div>
+                <div class="form-group">
+                  <label for="jenisRuangan">JENIS RUANGAN</label>
+                  <select name='id_area' class="form-control ruangan"></select>
+                </div>
+                <div class="form-group">
+                  <label for="jenisAlat">NAMA RUANGAN</label>
+                  <input type="text" name="nama_ruang" class="form-control" />
+                  <input type="hidden" name="id_ruang" class="form-control" />
+                </div>
+                <div class="form-group">
+                  <label for="panjangRuang">Panjang (m)</label>
+                  <input type="number" name="panjang" class="form-control" />
+                </div>
+                <div class="form-group">
+                  <label for="lebarRuang">Lebar (m)</label>
+                  <input type="number" name="lebar" class="form-control" />
+                </div>
+              
+                <div style="position:relative;">
+                          <a class='btn btn-primary' href='javascript:;'>
+                              Choose File...
+                              <input type="file" style='position:absolute;z-index:2;top:0;left:0;filter: alpha(opacity=0);-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";opacity:0;background-color:transparent;color:transparent;' name="file" size="40"  onchange='$("#upload-file-info").html($(this).val());'>
+                          </a>
+                          &nbsp;
+                          <span class='label label-info' id="upload-file-info"></span>
+                  </div>
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button class="btn btn-primary btnSimpan">Simpan</button>
+              </div>
+            </form>
+            </div>
+            <!-- /.chat -->
+           
+          </div>
+        </section>
+    
+      </div>
+   
+      <!-- /.row (main row) -->
+
+    </section>
+
+<?php $this->endsection();?>
+<?php $this->section('footer')?>
+<script type="text/javascript">
+  $('.btnSimpan').on('click',function(a){
+    a.preventDefault();
+    var dt = new FormData(document.getElementById('formTambahData'));
+    $.ajax({
+      type  : 'POST',
+      contentType: false,
+      contentType: false,  
+      cache: false,  
+      processData:false, 
+      url   : 'simpan',
+      data  : dt,
+      success: function(data){
+        if(data.success){
+          $.notify(data.pesan,'success');
+          setTimeout(function(){ window.location.href = '<?=base_url()?>/master/prasarana-ruang' }, 2000);
+        }else{
+          $.notify(data.pesan,'error');
+        }
+      }
+    });
+  });
+
+</script>
+
+<script type="text/javascript">
+$('.kompetensi_keahlian').select2({
+  ajax: {
+    type: 'POST',
+    url: '<?=base_url();?>/referensi/kompetensi_keahlian',
+    dataType: 'json'
+    // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+  }
+});
+$('.ruangan').select2({
+  ajax: {
+    type: 'POST',
+    url: '<?=base_url();?>/referensi/ruangan',
+    dataType: 'json'
+    // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+  }
+});
+
+</script>
+<?php $this->endsection();?>
