ab818e562675c24e11ec0ad1021857c9659ce287
Agus Wibowo <aguswibowo.amd@gmail.com>
2021-03-07 01:12:06 +0700

    ganti admin lte 3

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -148,7 +148,9 @@
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
-         
+          <div class="col-sm-6">
+           
+          </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
