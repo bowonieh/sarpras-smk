b2d1e9341aee7b5c76f8d5bdd404c4adb0d7b495
Agus Wibowo <aguswibowo.amd@gmail.com>
2021-03-06 22:42:44 +0700

    pesan

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -219,8 +219,7 @@
 <script src="<?php echo base_url();?>/vendor/almasaeed2010/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
 <!-- Slimscroll -->
 <script src="<?php echo base_url();?>/vendor/almasaeed2010/adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="<?php echo base_url();?>/vendor/almasaeed2010/adminlte/bower_components/fastclick/lib/fastclick.js"></script>
+
 <!-- AdminLTE App -->
 <script src="<?php echo base_url();?>/vendor/almasaeed2010/adminlte/dist/js/adminlte.min.js"></script>
 <script src="<?php echo base_url();?>/vendor/almasaeed2010/adminlte/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
