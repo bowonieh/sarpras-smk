ac1b38792099cd8243c88f90bb853ab5fd5b1c35
Agus Wibowo <aguswibowo.amd@gmail.com>
2021-03-07 01:11:35 +0700

    ganti admin lte 3

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -148,9 +148,7 @@
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Control Panel Dashboard</h1>
-          </div>
+         
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
